bfb988b3a36aeff193fee733d0e115b62e12b517
agnathanesg <agnarun@outlook.com>
2021-02-11 15:10:33 +0530

    Add files via upload

---
+++ NODE2_TMS/NODE2_TMS.ino
@@ -0,0 +1,77 @@
+
+#include "painlessMesh.h"
+#include "DHT.h"
+
+#define DHTTYPE DHT21
+
+// WiFi Credentials
+#define   MESH_PREFIX     "ABS_ACT"
+#define   MESH_PASSWORD   "Abs$12355"
+#define   MESH_PORT       5555
+uint8_t DHTPin = D2; 
+               
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);  
+
+Scheduler userScheduler; 
+painlessMesh  mesh;
+void sendMessage() ; 
+Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+void sendMessage() 
+{
+ // Serializing in JSON Format
+  DynamicJsonDocument doc(1024);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  doc["TEMP2"] = t;
+  doc["HUM2"] = h;
+  String msg ;
+  serializeJson(doc, msg);
+  mesh.sendBroadcast( msg );
+  Serial.println("from node2");
+  Serial.println(msg);
+  taskSendMessage.setInterval((TASK_SECOND * 10));
+}
+void receivedCallback( uint32_t from, String &msg ) {
+  String json;
+  DynamicJsonDocument doc(1024);
+  json = msg.c_str();
+  DeserializationError error = deserializeJson(doc, json);
+  if (error)
+  {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+  }
+   String Temp = doc["TEMP"];
+   String Hum = doc["HUM"];
+  Serial.println("From node1");
+   Serial.println("Temperature:");
+   Serial.print(Temp);
+    Serial.println("Humidity:");
+   Serial.print(Hum);
+}
+void newConnectionCallback(uint32_t nodeId) {
+  Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+}
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
+}
+void setup() {
+  Serial.begin(115200);
+ pinMode(DHTPin, INPUT);
+  dht.begin();
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
+}
+void loop() {
+  mesh.update();
+}
